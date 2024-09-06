2bc340759da31da2c935073a0de7934dea0fb8a0
Yoshi Odei <yoshiodei@yahoo.com>
2024-09-06 21:45:55 +0000

    modify farmer data

--- src/components/farmerDetailCard.jsx
+++ src/components/farmerDetailCard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FiEdit3 } from 'react-icons/fi'
+import { regionsArray } from '../config';
 
 const FarmerDetailCard = ({ farmerData, f7router }) => {
-    const {name, gender, phone, yearOfBirth, yearsOfExperience} = farmerData;
-    
+    const {
+      firstname, 
+      lastname, 
+      gender, 
+      mobilenumber, 
+      community, 
+      network, 
+      experience_year,
+      idcardnumber,
+      idcardtype,
+    } = farmerData;
+
+    console.log('farmerData raahhh raaww', farmerData);
+
     return (
         <div className="bg-blue-100 rounded-lg p-3 mb-4">
           <div className="flex justify-between items-center w-full">
@@ -20,23 +33,43 @@ const FarmerDetailCard = ({ farmerData, f7router }) => {
           <div className="flex flex-col gap-y-2">
             <div className="flex justify-between items-center h-[1.2em]">
               <h6>Name:</h6>
-              <p>{name}</p>
+              <p>{`${firstname} ${lastname}`}</p>
             </div>
             <div className="flex justify-between items-center h-[1.2em]">
               <p>Gender:</p>
               <p>{gender}</p>
             </div>
             <div className="flex justify-between items-center h-[1.2em]">
+              <p>Network:</p>
+              <p>{network}</p>
+            </div>
+            <div className="flex justify-between items-center h-[1.2em]">
               <p>Phone:</p>
-              <p>{phone}</p>
+              <p>{mobilenumber}</p>
             </div>
             <div className="flex justify-between items-center h-[1.2em]">
-              <p>Year of birth:</p>
-              <p>{yearOfBirth}</p>
+              <p>Community:</p>
+              <p>{community?.split(', ')[1]}</p>
             </div>
             <div className="flex justify-between items-center h-[1.2em]">
-              <p>Years of experience:</p>
-              <p>{yearsOfExperience}</p>
+              <p>District:</p>
+              <p>{community?.split(', ')[2]}</p>
+            </div>
+            <div className="flex justify-between items-center h-[1.2em]">
+              <p>Region:</p>
+              <p>{community?.split(', ')[3]}</p>
+            </div> 
+            <div className="flex justify-between items-center h-[1.2em]">
+              <p>Year Began:</p>
+              <p>{experience_year}</p>
+            </div>
+            <div className="flex justify-between items-center h-[1.2em]">
+              <p>ID Card Type</p>
+              <p>{idcardtype ? idcardtype : 'N/A'}</p>
+            </div>
+            <div className="flex justify-between items-center h-[1.2em]">
+              <p>ID Card Number</p>
+              <p>{idcardnumber ? idcardnumber : 'N/A'}</p>
             </div>
           </div>
         </div>
