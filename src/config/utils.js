790bff92020e97d201ed1a011cdba54c9df232d9
Yoshi Odei <yoshiodei@yahoo.com>
2024-09-25 18:39:08 +0000

    modify OTP function

--- src/config/utils.js
+++ src/config/utils.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const getYearArray = () => {
     const currentYear = new Date().getFullYear();
     const years = [];
@@ -6,3 +8,39 @@ export const getYearArray = () => {
     }
     return years;
 }  
+
+export const sendOTPToPhone = async (phone) => {
+    axios.post(
+        'https://torux.app/api/agentsignIn',
+        { mobile: phone }, // "0202944211"
+        {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        }
+    )
+        .then(function (response) {
+
+        if (response.status === 200) {
+            const responseData = response.data;
+            console.log("Response Data:", responseData);
+            console.log("Error:", responseData.error);
+            console.log("Message:", responseData.message);
+        } else {
+            console.log("Unexpected status code:", response.status);
+        }
+        })
+        .catch(function (error) {
+        if (error.response) {
+
+            console.log("Error Status Code:", error.response.status);
+            console.log("Error Data:", error.response.data);
+        } else if (error.request) {
+
+            console.log("No response received:", error.request);
+        } else {
+
+            console.log("Error", error.message);
+        }
+        });
+}
