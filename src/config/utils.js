05d63782e4ff0225b75e33ea67f83929a02fb306
Yoshi Odei <yoshiodei@yahoo.com>
2024-11-02 16:17:45 +0000

    add push notification functions

--- src/config/utils.js
+++ src/config/utils.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { PushNotifications } from '@capacitor/push-notifications';
 
 export const getYearArray = () => {
     const currentYear = new Date().getFullYear();
@@ -44,3 +45,63 @@ export const sendOTPToPhone = async (phone) => {
         }
         });
 }
+
+export const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June", 
+        "July", "August", "September", "October", "November", "December"
+    ];
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear();
+
+    // Determine the suffix for the day
+    const suffix = (day % 10 === 1 && day !== 11) ? "st" :
+                   (day % 10 === 2 && day !== 12) ? "nd" :
+                   (day % 10 === 3 && day !== 13) ? "rd" : "th";
+
+    return `${day}${suffix} ${month}, ${year}`;
+}
+
+export const trimStringToLength = (number, str) => {
+    if (str.length <= number) {
+        return str; // If the string length is less than or equal to the number, do nothing
+    }
+    return str.slice(0, number) + '...'; // Cut the string and add '...'
+}
+
+export const registerPushNotifications = () => {
+    PushNotifications.requestPermissions().then((permission) => {
+        if (permission.receive === 'granted') {
+            PushNotifications.register();
+        } else {
+            console.log("Push notification permission not granted");
+        }
+    });
+};
+
+export const setupPushNotificationListeners = () => {
+    // Get the device ID (FCM token)
+    PushNotifications.addListener('registration', (token) => {
+        console.log('Device FCM Token:', token.value);
+        // Send this token to the server for sending notifications so you can add to when the User request for OTP
+    });
+
+    // Handle registration errors
+    PushNotifications.addListener('registrationError', (error) => {
+        console.error('Registration error:', error);
+    });
+
+    // Handle incoming notifications
+    PushNotifications.addListener('pushNotificationReceived', (notification) => {
+        console.log('Notification received:', notification);
+        // Display notification content or trigger Framework7 alert
+    });
+
+    // Handle notification tap action
+    PushNotifications.addListener('pushNotificationActionPerformed', (notification) => {
+        console.log('Notification tapped:', notification);
+        // Navigate to specific pages based on notification data
+    });
+};
